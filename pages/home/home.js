8354818c0c5257a60aada75231dc11c1739b9cf9
2182025547 <2182025547@qq.com>
2020-08-23 09:14:23 +0800

    Initial Commit

---
+++ pages/home/home.js
@@ -0,0 +1,124 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //中间板块的数据x 
+    x:[
+      {
+        big:"现在下单",
+        small:"大师咖啡/瑞纳冰"
+      },
+      {
+        big:"瑞纳冰展览会",
+        small:"杨枝甘露/牛油果/杨梅"
+      }
+    ],
+    //中间板块的数据m
+    m:[
+      {
+        text:"瑞幸潮品"
+      },
+      {
+        text:"瑞划算"
+      },
+      {
+        text:"我的优惠券"
+      }
+    ],
+    //中间板块的数据l
+    l:[
+      {
+        text:"咖啡钱包"
+      },
+      {
+        text:"送TA咖啡"
+      }
+    ],
+    mydata2:[
+      {
+        image:"../../images/c1.jpg",
+        mark:"潮品自营",
+        info:"LJISJDIFJLJJ咖啡",
+        price:"￥32",
+        oldprice:"￥40",
+        icon:"../../images/click.png"
+      },
+      {
+        image:"../../images/c2.jpg",
+        mark:"潮品自营",
+        info:"LJISJDIFJLJJ咖啡",
+        price:"￥35",
+        oldprice:"￥60",
+        icon:"../../images/click.png"
+      },
+      {
+        image:"../../images/c3.jpg",
+        mark:"潮品自营",
+        info:"LJISJDIFJLJJ咖啡",
+        price:"￥44",
+        oldprice:"￥63",
+        icon:"../../images/click.png"
+      },
+     
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
