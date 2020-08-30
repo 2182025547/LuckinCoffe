4ce55d290fe3362dbe88e8273c6f04c25b6649f8
2182025547 <2182025547@qq.com>
2020-08-30 19:01:09 +0800

    sss

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,49 @@
-//index.js
-//获取应用实例
-const app = getApp()
+
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+   mydata:[
+     {
+       info:"0",
+       a:"元",
+       ts:"账户余额"
+     },
+     {
+      info:"0",
+      a:"张",
+      ts:"咖啡钱包"
+    },
+    {
+      info:"4",
+      a:"张",
+      ts:"优惠卷"
+    },
+    {
+      info:"0",
+      a:"张",
+      ts:"礼品卡"
+    },
+
+   ],
+   mydata2:[
+     {
+       image:"order.png",
+       info:"发票管理 "
+     },
+     {
+      image:"message.png",
+      info:"通知管理"
+    },
+    {
+      image:"like.png",
+      info:"兑换优惠"
+    },
+    {
+      image:"customer_service.png",
+      info:"客户服务"
     }
+   ]
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  
+    
 })
